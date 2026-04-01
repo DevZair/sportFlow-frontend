9a081094f80eb72ecdadeacf84e9738917df06d2
DevZair <zoir3201@gmail.com>
2026-04-01 11:24:05 +0500

    new fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: 'https://sportflow-backend.onrender.com/api',
 });
 
 // Add interceptor to add token to headers
